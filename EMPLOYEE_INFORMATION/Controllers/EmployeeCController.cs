ca47b2bf7377b74c17d42c595ecddef668b23401
BineeshM2H <bineesh@m2hinfotech.com>
2025-02-24 13:14:24 +0530

    SaveDependentEmp Api Attribute changed

--- EMPLOYEE_INFORMATION/Controllers/EmployeeCController.cs
+++ EMPLOYEE_INFORMATION/Controllers/EmployeeCController.cs
@@ -20,58 +20,58 @@ namespace EMPLOYEE_INFORMATION.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> FillTravelType ()
-            {
-            var fillTravelType = await _employeeInformationC.FillTravelType ();
-            return new JsonResult (fillTravelType);
-            }
+        public async Task<IActionResult> FillTravelType()
+        {
+            var fillTravelType = await _employeeInformationC.FillTravelType();
+            return new JsonResult(fillTravelType);
+        }
         [HttpGet]
-        public async Task<IActionResult> FillEmployeesBasedOnRole (int firstEntityId, int secondEntityId,string transactionType)
-            {
-            var fillEmployeesBasedOnRole = await _employeeInformationC.FillEmployeesBasedOnRole (firstEntityId, secondEntityId, transactionType);
-            return new JsonResult (fillEmployeesBasedOnRole);
-            }
-        [HttpGet]   
-        public async Task<IActionResult> GetDependentDetails (int employeeId)
-            {
-            var getDependentDetails = await _employeeInformationC.GetDependentDetails (employeeId);
-            return new JsonResult (getDependentDetails);
-            }
+        public async Task<IActionResult> FillEmployeesBasedOnRole(int firstEntityId, int secondEntityId, string transactionType)
+        {
+            var fillEmployeesBasedOnRole = await _employeeInformationC.FillEmployeesBasedOnRole(firstEntityId, secondEntityId, transactionType);
+            return new JsonResult(fillEmployeesBasedOnRole);
+        }
         [HttpGet]
-        public async Task<IActionResult> SaveDependentEmp ([FromBody] SaveDependentEmpDto SaveDependentEmp)   // For Document and Bank Insertion
-            {
-            var saveDependentEmp = await _employeeInformationC.SaveDependentEmp (SaveDependentEmp);
+        public async Task<IActionResult> GetDependentDetails(int employeeId)
+        {
+            var getDependentDetails = await _employeeInformationC.GetDependentDetails(employeeId);
+            return new JsonResult(getDependentDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveDependentEmp([FromBody] SaveDependentEmpDto SaveDependentEmp)   // For Document and Bank Insertion
+        {
+            var saveDependentEmp = await _employeeInformationC.SaveDependentEmp(SaveDependentEmp);
 
-            return Ok (saveDependentEmp);
-            }
+            return Ok(saveDependentEmp);
+        }
         [HttpGet]
-        public async Task<IActionResult> RetrieveEducation ( )
-            {
-            var retrieveEducation = await _employeeInformationC.RetrieveEducation ( );
-            return new JsonResult (retrieveEducation);
-
-            }
-        [HttpGet]
-        public async Task<IActionResult> RetrieveCourse ( )
-            {
-            var retrieveCourse = await _employeeInformationC.RetrieveCourse ( );
-            return new JsonResult (retrieveCourse);
-
-            }
-        [HttpGet]
-        public async Task<IActionResult> RetrieveSpecial ( )
-            {
-            var retrieveSpecial = await _employeeInformationC.RetrieveSpecial ( );
-            return new JsonResult (retrieveSpecial);
-
-            }
-        [HttpGet]
-        public async Task<IActionResult> RetrieveUniversity ( )
-            {
-            var retrieveUniversity = await _employeeInformationC.RetrieveUniversity ( );
-            return new JsonResult (retrieveUniversity);
-
-            }
+        public async Task<IActionResult> RetrieveEducation()
+        {
+            var retrieveEducation = await _employeeInformationC.RetrieveEducation();
+            return new JsonResult(retrieveEducation);
 
         }
+        [HttpGet]
+        public async Task<IActionResult> RetrieveCourse()
+        {
+            var retrieveCourse = await _employeeInformationC.RetrieveCourse();
+            return new JsonResult(retrieveCourse);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> RetrieveSpecial()
+        {
+            var retrieveSpecial = await _employeeInformationC.RetrieveSpecial();
+            return new JsonResult(retrieveSpecial);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> RetrieveUniversity()
+        {
+            var retrieveUniversity = await _employeeInformationC.RetrieveUniversity();
+            return new JsonResult(retrieveUniversity);
+
+        }
+
+    }
 }
