29ba108e7be26833dcc34daeba8992cd38665fd4
BineeshM2H <bineesh@m2hinfotech.com>
2025-03-21 10:03:51 +0530

    Attendance Project Added

---
+++ HRMS.EmployeeInformation.Models/Models/Entity/UserType.cs
@@ -0,0 +1,10 @@
+﻿namespace HRMS.EmployeeInformation.Models.Models.Entity;
+
+public partial class UserType
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Code { get; set; }
+}
