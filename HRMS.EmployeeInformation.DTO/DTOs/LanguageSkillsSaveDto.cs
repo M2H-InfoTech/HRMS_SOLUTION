c8c22bde952f7a6b6a8df1f50b12f5b1c2a5411c
BineeshM2H <bineesh@m2hinfotech.com>
2025-03-27 17:50:29 +0530

    Language Skills InsertUpdate fixed

--- HRMS.EmployeeInformation.DTO/DTOs/LanguageSkillsSaveDto.cs
+++ HRMS.EmployeeInformation.DTO/DTOs/LanguageSkillsSaveDto.cs
@@ -4,7 +4,6 @@
     {
 
         public int EmpID { get; set; }
-        public string? Type { get; set; }
         public List<LanguageSkillDto>? Lilanguage { get; set; } // Ensure this is a list
     }
 
