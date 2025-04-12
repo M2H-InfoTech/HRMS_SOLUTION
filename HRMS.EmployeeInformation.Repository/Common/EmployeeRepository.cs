48993776b26405805d65b47980a04d4e5adc046c
gouri@officekithr.com <gouri@officekithr.com>
2025-04-12 10:59:12 +0530

    CHANGES FOR RETRIEVAL CERTIFICATIONS

--- HRMS.EmployeeInformation.Repository/Common/EmployeeRepository.cs
+++ HRMS.EmployeeInformation.Repository/Common/EmployeeRepository.cs
@@ -1815,7 +1815,7 @@ namespace HRMS.EmployeeInformation.Repository.Common
 
             // Fetch employee certifications
             var certifications = await _context.EmployeeCertifications
-                .Where(ec => ec.EmpId == employeeId && ec.Status !="D")
+                .Where(ec => ec.EmpId == employeeId && ec.Status !=_employeeSettings.LetterD)
                 .Select(ec => new CertificationDto
                 {
                     empId = ec.EmpId,
