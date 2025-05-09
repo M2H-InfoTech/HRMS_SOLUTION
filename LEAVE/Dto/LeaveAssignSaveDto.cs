0f5fd78b451fbc5a1c101bbefad071539873c4dc
Shan-M2H <shanlal@m2hinfotech.com>
2025-05-09 12:03:54 +0530

    LeaveAssign Changes Again For Safety

---
+++ LEAVE/Dto/LeaveAssignSaveDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEAVE.Dto
+{
+    public class LeaveAssignSaveDto
+    {
+        public string LeaveMasters { get; set; }
+        public string EmployeeIds { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime? ValidTo { get; set; }
+        public int EntryBy { get; set; }
+        public int LinkLevel { get; set; }
+        public int LevelId { get; set; }
+    }
+}
--- LEAVE/Service/AssignLeave/IAssignLeaveService.cs
+++ LEAVE/Service/AssignLeave/IAssignLeaveService.cs
@@ -8,6 +8,9 @@ namespace LEAVE.Service.AssignLeave
         Task<List<BsemployeedataDto>> Bsemployeedata(int employeeId);
         Task<List<FillchildBSdetailsDto>> FillchildBSdetails(int employeeId);
         Task<object> Getallbasics(string linkid, int levelid, string transaction, int empid);
+        Task<Object> GetBasicAssignmentAsync (int roleId, int entryBy);
+        Task<bool> DeleteSingleEmpBasicSettingAsync (int leavemasters, int empid);
+        Task<int> AssignBasicsAsync (LeaveAssignSaveDto Dto);
 
     }
 }
