90417bacb796abf089b099620b7b48c729600d9f
AsifM2H <asif@m2hinfotech.com>
2025-05-22 11:53:23 +0530

    test

--- LEAVE/Controllers/AssignLeave/AssignLeaveController.cs
+++ LEAVE/Controllers/AssignLeave/AssignLeaveController.cs
@@ -63,7 +63,7 @@ namespace LEAVE.Controllers.AssignLeave
                 return StatusCode (500, "An error occurred while processing your request.");
             }
         }
-        [HttpPost]
+        [HttpPost] //cerated by Shan Lal K
         public async Task<IActionResult> AssignBasicsAsync ([FromBody] LeaveAssignSaveDto Dto)
         {
             if (Dto == null)
